3ced7beca363330d48e9cea06699c6764b326a87
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2020-04-23 13:25:40 -0400

    updates to config parsing

--- bin/config.py
+++ bin/config.py
@@ -8,11 +8,14 @@ import ConfigParser as parser
 MAXSTRING = 40
 
 # To insure an exact match between struct definitions,
-# the SettingsStruct definition must be edited manually
+# the TEMPLATE struct definition must be edited manually
 # in this file and src/utils.c before recompiling projectmanager.
+TEMPLATE = [("git", ctypes.c_int)]
+
 class SettingsStruct(ctypes.Structure):
     global MAXSTRING
-    _fields_ = [("git", ctypes.c_int)]
+    global TEMPLATE
+    _fields_ = TEMPLATE
 
 class SettingsData:
 
@@ -25,11 +28,34 @@ class SettingsData:
         # self.settings_struct will be initialized and populated
         # once user settings are loaded into self.settings_dict
         self.settings_struct = None # SettingsStruct(values)
-        
+
+    def read_input(self, config):
+        error_count = 0
+        for field in config.keys():
+            try:
+                self.settings_dict[field] =\
+                type(self.settings_dict[field])(config[field])
+                
+            except KeyError:
+                error_count += 1
+                print "Invalid field", field, "not read."
+                
+            except ValueError:
+                error_count += 1
+                print "Config value", config[field],\
+                    "could not be converted to type",\
+                    type(self.settings_dict[field]).__name__,\
+                    "for field", field + "."
+                print "Maintaining default value",\
+                    self.settings_dict[field] + "."
+                
+        return error_count
+
+######################################################
 
 def writeb_open(filename):
     try:
-        return open(filename, "wb")
+        return open(filename, "rb")
     except:
         print "Error opening internal config file at", filename
         exit(1)
@@ -47,7 +73,7 @@ def py_to_ctype(py_var):
         else:
             return (ctypes.c_char * MAXSTRING)
     else:
-        print "Error: settings variable type error at:"
+        print "Error: unexpected settings variable type error at:"
         print py_var
         exit(1)
 
@@ -69,45 +95,16 @@ def post_parse(config_obj):
             config_dict[section] = config_obj.items(section)
     return config_dict
 
-############################
-
-def print_commands(command_string):
-    command_list = command_string.split('|||')
-    for command in command_list:
-        print "$$", command.lstrip()
-
-def print_values(dict_item):
-    commands_index = None
-    for values in dict_item:
-        if values[0] != 'commands':
-            print "{} = {}".format(values[0], values[1])
-        else:
-            commands_index = dict_item.index(values)
-    if commands_index != None:
-        print_commands(dict_item[commands_index][1])
-    print "--"
-
-def print_rest(config_dict):
-    for section in config_dict.keys():
-        if (section != 'settings') and (section != 'modes'):
-            print "% {}".format(section)
-            print_values(config_dict[section])
-        elif section == 'modes':
-            for mode in config_dict['modes'].keys():
-                    print "%% {}".format(mode)
-                    print_values(config_dict['modes'][mode])
-        elif section == 'settings':
-            pass
-
-def dump_config(config_dict):
-    print headerstring
-    print "%%% settings"
-    print_values(config_dict['settings'])
-    print_rest(config_dict)
-
-##############################
+########################################
 
 def main(conf_path, iconf_path):
+    config_dict = post_parse(parse(conf_path))
+    settings_dict = {i : config_dict[i] for i in config_dict\
+                     if i != 'modes'}
+    settings = SettingsData()
+    rc = settings.read_input(settings_dict)
+    if rc:
+        print "Read config with", rc, "errors."
     iconf_file = writeb_open(iconf_path)
     return 0
 
