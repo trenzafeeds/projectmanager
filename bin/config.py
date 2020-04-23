5229bb3b4d33a7924a9d767fe903cd3ef60a1d02
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2020-04-23 19:46:24 -0400

    writing binary sruct to file

--- bin/config.py
+++ bin/config.py
@@ -8,10 +8,12 @@ import ConfigParser as parser
 MAXSTRING = 40
 
 # To insure an exact match between struct definitions,
-# the TEMPLATE struct definition must be edited manually
+# the STRUCT_FORM struct definition must be edited manually
 # in this file and src/utils.c before recompiling projectmanager.
-TEMPLATE = [("git", ctypes.c_int)]
-
+STRUCT_FORM = [(("git", ctypes.c_int), 1)]
+TEMPLATE = [ option[0] for option in STRUCT_FORM ]
+DEFAULTS = { option[0][0] : option[1] for option in STRUCT_FORM }
+ 
 class SettingsStruct(ctypes.Structure):
     global MAXSTRING
     global TEMPLATE
@@ -23,11 +25,12 @@ class SettingsData:
         # self.settings_dict is initialized with the DEFAULT
         # values for each setting. Default values are also
         # used to typecheck user-entered values.
-        self.settings_dict = {"git" : 0}
+        global DEFAULTS
+        self.settings_dict = DEFAULTS
         
         # self.settings_struct will be initialized and populated
         # once user settings are loaded into self.settings_dict
-        self.settings_struct = None # SettingsStruct(values)
+        self.struct_obj = None # SettingsStruct(values)
 
     def read_input(self, config):
         error_count = 0
@@ -35,6 +38,8 @@ class SettingsData:
             try:
                 self.settings_dict[field] =\
                 type(self.settings_dict[field])(config[field])
+                if type(self.settings_dict[field]) is str:
+                        self.settings_dict[field].encode('utf-8')
                 
             except KeyError:
                 error_count += 1
@@ -48,14 +53,30 @@ class SettingsData:
                     "for field", field + "."
                 print "Maintaining default value",\
                     self.settings_dict[field] + "."
+
+            except:
+                error_count += 1
+                print "Unexpected error at", field + "."
                 
         return error_count
 
+    def gen_struct(self):
+        global TEMPLATE
+        struct_args = []
+        for field in TEMPLATE:
+            struct_args.append(self.settings_dict[field[0]])
+        try:
+            self.struct_obj = SettingsStruct(*struct_args)
+        except:
+            print "Error creating ctypes setting struct."
+            exit(1)
+        return 0
+    
 ######################################################
 
 def writeb_open(filename):
     try:
-        return open(filename, "rb")
+        return open(filename, "wb")
     except:
         print "Error opening internal config file at", filename
         exit(1)
@@ -77,11 +98,19 @@ def py_to_ctype(py_var):
         print py_var
         exit(1)
 
+def options_dict(parser, section):
+    optionsd = {}
+    for option in parser.options(section):
+        try:
+            if parser.getboolean(section, option): optionsd[option] = 1
+            else: optionsd[option] = 0
+        except ValueError:
+            optionsd[option] = parser.get(section, option)
+    return optionsd
+
 def parse(path):
     config_parsed = parser.ConfigParser()
-    try:
-        config_parsed.read(path)
-    except:
+    if not config_parsed.read(path):
         print "Error reading configuration file at", path
         exit(1)
     return config_parsed
@@ -92,20 +121,22 @@ def post_parse(config_obj):
         if section[0:5] == 'mode/':
             config_dict['modes'][section[5:]] = config_obj.items(section)
         else:
-            config_dict[section] = config_obj.items(section)
+            config_dict[section] = options_dict(config_obj, section)
     return config_dict
 
 ########################################
 
 def main(conf_path, iconf_path):
     config_dict = post_parse(parse(conf_path))
-    settings_dict = {i : config_dict[i] for i in config_dict\
-                     if i != 'modes'}
     settings = SettingsData()
-    rc = settings.read_input(settings_dict)
-    if rc:
-        print "Read config with", rc, "errors."
+    rc = settings.read_input(config_dict['settings'])
+    print "Read config with", rc, "errors."
+    settings.gen_struct()
     iconf_file = writeb_open(iconf_path)
+    print "Writing updated configuration to iconf file..."
+    iconf_file.write(settings.struct_obj)
+    iconf_file.flush()
+    iconf_file.close()
     return 0
 
 if __name__ == "__main__":
